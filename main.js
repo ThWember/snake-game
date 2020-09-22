371c03964357afeb5d3d9d7cb1dd4ffe0bd8373b
Thais Wemberlaine <thaiswemberlaine@gmail.com>
2020-09-21 22:59:23 -0300

    Snake body

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 16;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 function criarbg() {
     context.fillStyle = "green";
     context.fillRect(0, 0, 32 * box, 32 * box);
 }
 
+function snakebody() {
+    for(i=0; i < snake.length; i++) {
+        context.fillStyle = "gray";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
 criarbg();
+snakebody();
