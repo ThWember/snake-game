e6e4d35d1d700f420c98912ff5a08314c6f73ffc
Thais Wemberlaine <thaiswemberlaine@gmail.com>
2020-09-24 16:25:21 -0300

    Updated keycode direction

--- main.js
+++ main.js
@@ -43,6 +43,22 @@ function update (event) {
     if(event.keyCode == 40 && direction!= "up") {
         direction = "down";
     }
+
+    if(event.keyCode == 65 && direction!= "right") {
+        direction = "left";
+    }
+
+    if(event.keyCode == 87 && direction!= "down") {
+        direction = "up";
+    }
+
+    if(event.keyCode == 68 && direction!= "left") {
+        direction = "right";
+    }
+
+    if(event.keyCode == 83 && direction!= "up") {
+        direction = "down";
+    }
 }
 
 function startGame() {
